6d0261e89b41c78e1556c7e02690d5344c0f5890
DarkoPg27 <darkobrnovic27@gmail.com>
2024-10-18 13:54:09 +0200

    city view layout

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -106,5 +106,36 @@ const weatherData = await getWeatherData();
         </div>
       </div>
     </div>
+
+    <hr class="border-white border-opacity-10 border w-full" />
+
+    <!-- Weekly Weather -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">7 Day forecast</h2>
+        <div
+          v-for="day in weatherData.daily"
+          :key="day.dt"
+          class="flex item-center"
+        >
+          <p class="flex-1">
+            {{
+              new Date(day.dt * 1000).toLocaleDateString("en-us", {
+                weekday: "long",
+              })
+            }}
+          </p>
+          <img
+            class="w-[50px] h-[50px] object-cover"
+            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+            alt=""
+          />
+          <div class="flex gap-2 flex-1 justify-end">
+            <p>H:{{ Math.round(day.temp.max) }}</p>
+            <p>L:{{ Math.round(day.temp.min) }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
